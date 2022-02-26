1fca6a68f5a0b3eaa68067f67ccd945e8a08392e
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-26 12:25:04 -0500

    making styling framework

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,10 @@ import Reggi from '../images/icons/reggi27.png'
 export default function NavBar() {
   return (
     <nav className="text-black App-header App-link">
-    <img src={Reggi} className="App-logo" alt="logo" /> 
-    <a href="/">Back to Root</a>
-    <ul className="flex items-center">
+    <img src={Reggi} className="App-logo " alt="logo" /> 
+    <a href="/">Home</a>
+    
+    <ul className="flex items-center ">
       <li><a href="https://github.com/Cheez0id">Github</a></li>
       <li><a href="https://github.com/Cheez0id/mary-prince">To Repo</a></li>
       <li><a href="https://github.com/Cheez0id">Github</a></li>
