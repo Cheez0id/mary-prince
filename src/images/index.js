5c10d76389f4fbc2d962ee87604f8f1dd958e9ae
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-26 19:43:19 -0500

    added images

--- src/images/index.js
+++ src/images/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 const images = {
-  logoPic: require('./profileLogo.png'),
+  logoPic: require('./logoPic.png'),
   cookies: require('./cookies.png'),
   foodFinder: require('./foodFinder.jpg'),
   hubble: require('./hubbledeep.png'),
   insta: require('./instagram.png'),
   logoBw: require('./logoBw.png'),
+  logoMp: require('./logoMp.png'),
+  octoCat: require('./Octocat.png'),
+  penguin: require('./penguin.png'),
+  port1: require('./port1.jpg'),
   reggi: require('./reggi.png'),
-
-
-
+  resume: require('./resume.jpg'),
+  signature: require('./signature.png'),
+  snake: require('./snake.png'),
+  traditional: require('./traditional.jpg'),
+  workDay: require('./workDay.jpg'),
 }
 
 export default images;
