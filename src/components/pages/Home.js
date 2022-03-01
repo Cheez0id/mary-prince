5d2c569e12d023760118fa91d62a03696d0431be
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-03-01 11:11:51 -0500

    styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ import images from "../../images/index";
 
 export default function Home() {
   return (
-    <div className= "App-body bg-cover bg-fixed">
+    <div className= "App-body longbox bg-cover bg-fixed">
    something soemthing
     </div>
   )
