9d9c8bbbcba527ab24304baa5362b81f873c117f
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-03-01 10:42:15 -0500

    resume button

--- src/components/pages/NavBar.js
+++ src/components/pages/NavBar.js
@@ -12,37 +12,39 @@ function NavBar({ currentPage, handlePageChange }) {
 						href="#home"
 						onClick={() => handlePageChange("Home")}
 					>
-						<img src={images.logoPic2} className="App-logo" alt="marylogo" width="100"/><span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500 hover:no-underline font-bold text-2xl lg:text-2xl">
+						<img src={images.logoPic2} className="App-logo" alt="marylogo" width="100"/><span class="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 via-pink-500 to-purple-500 hover:no-underline font-bold text-2xl lg:text-2xl">
               Mary Prince
             </span>
           </a>
 				</li>
 				
-				<li className="Nav-links">
+				<li className="Nav-links font-bold">
 					<a href="#AboutMe" onClick={() => handlePageChange("AboutMe")}>
 						About Me*
 					</a>
 				</li>
-				<li className="Nav-links">
+				<li className="Nav-links font-bold">
         <a href="#Portflio" onClick={() => handlePageChange("Portfolio")}>
 						Portfolio
 					</a>
          </li> 
-          <li className="Nav-links">
+          <li className="Nav-links font-bold">
         <a href="#Contact" onClick={() => handlePageChange("Contact")}>
 						Contact
 					</a>
 				</li>
-        <li className="Nav-links">
+        <li className="Nav-links font-bold">
 					<a href="https://github.com/Cheez0id">Github</a>
 				</li>
 				<li className="Nav-links">
 					<a href="https://cheez0id.github.io/MaryLeePrince-portfolio1/">Original Portfolio*</a>
 				</li>
 				<li className="Nav-links">
-					<a href={resume} download="Resume-MLP-1pg.pdf"><span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
-              Download Resume
-            </span>
+					<a href={resume} download="Resume-MLP-1pg.pdf">
+                          
+						<button class="bg-gradient-to-r from-purple-800 to-teal-500 hover:from-pink-500 hover:to-teal-500 text-white font-bold py-2 px-2 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out" type="button">
+                Download Resume
+              </button>
 					</a>
 				</li>
 			</ul>
