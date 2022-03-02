f491672f0f486ce67f4b98c0fe72433807d03f6b
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-03-02 13:07:01 -0500

    styling

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -13,7 +13,7 @@ export default function Portfolio() {
                <a className="App-link inline-flex 
                transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300" href="https://epimoodtracker.herokuapp.com/"><img src={images.epiMood} className="shadow-lg inlineImage" alt="epimoodlogo" width="250"/>
                 </a><br></br>
-                <a className="App-link-repo" href="https://github.com/Cheez0id/EpiMood" alt="repo">> Repo</a>
+                <button className=' transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'><a className="App-link-repo" href="https://github.com/Cheez0id/EpiMood" alt="repo">> Repo</a></button>
                 </card>
                 <iframe className='inlineImage' src="https://drive.google.com/file/d/14flS4Oj-f50S8AWWzbh7lSEEbSpgph-c/preview" width="640" height="480" allow="autoplay"></iframe>
                 <card >
@@ -22,7 +22,7 @@ export default function Portfolio() {
             </span></h1>
                <a className="App-link inline-flex transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300" href="https://epimoodtracker.herokuapp.com/"><img src={images.logoMp} className="shadow-lg inlineImage" alt="epimoodlogo" width="200"/>
                 </a><br></br>
-                <a className="App-link-repo" href="https://cheez0id.github.io/MaryLeePrince-portfolio1/" alt="repo">> Repo</a>
+                <button className=' transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'><a className="App-link-repo" href="https://cheez0id.github.io/MaryLeePrince-portfolio1/" alt="repo">> Repo</a></button>
                 </card>
                 
              
@@ -34,11 +34,11 @@ export default function Portfolio() {
             </span></h1>
               <card className="box"> <a className="App-link inline-flex transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300" href="https://cookies-for-coders-mary.herokuapp.com"><img src={images.cookies} className="shadow-lg inlineImage" alt="cookies" width="200"/>
                 </a><br></br>
-                <a className="App-link-repo" href="https://github.com/Cheez0id/Full_Stack_Store" alt="repo">> Repo</a></card>
+                <button className=' transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'><a className="App-link-repo" href="https://github.com/Cheez0id/Full_Stack_Store" alt="repo">> Repo</a></button></card>
 
                 <card className="box"> <a className="App-link inline-flex transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300" href="https://cheez0id.github.io/Charity-Locator-Project-P1G7"><img src={images.foodFinder} className="shadow-lg inlineImage" alt="charitylocator" width="200"/>
                 </a><br></br>
-                <a className="App-link-repo" href="https://github.com/Cheez0id/Full_Stack_Store" alt="repo">> Repo</a></card>
+                <button className=' transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'><a className="App-link-repo" href="https://github.com/Cheez0id/Full_Stack_Store" alt="repo">> Repo</a></button></card>
                 
            </section>
 
@@ -49,15 +49,15 @@ export default function Portfolio() {
             </span></h1>
             <card className="box">  <a className="App-link inline-flex transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300" href="https://drive.google.com/file/d/15JwRFydPZ_h_6FmCOo9AcucGXar4Po4K/view?usp=sharing"><img src={images.social} className="shadow-lg inlineImage" alt="socialnetwork" width="200"/>
                 </a>       <br></br>     
-               <a className="App-link-repo" href="https://github.com/Cheez0id/social_network_API" alt="repo">> Repo</a></card>
+                <button className=' transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'><a className="App-link-repo" href="https://github.com/Cheez0id/social_network_API" alt="repo">> Repo</a></button></card>
                <card className="box">
                <a className="App-link inline-flex transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300" href="https://cheez0id.github.io/Weather-Dashboard-Homework"><img src={images.weather} className="shadow-lg inlineImage" alt="weather dashboard" width="200"/>
                 </a><br></br>
-                <a className="App-link-repo" href="https://github.com/Cheez0id/Weather-Dashboard-Homework" alt="repo">> Repo</a></card>
+                <button className=' transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'><a className="App-link-repo" href="https://github.com/Cheez0id/Weather-Dashboard-Homework" alt="repo">> Repo</a></button></card>
           
                 <card className="box"> <a className="App-link inline-flex transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300" href="https://drive.google.com/file/d/1NSTo07dxxTOB-uQCsocjfNLQFtiy8Y08/view?usp=sharing"><img src={images.employee} className="shadow-lg inlineImage" alt="employeeCMS" width="200"/>
                 </a><br></br>
-                <a className="App-link-repo" href="https://github.com/Cheez0id/Employee_CMS" alt="repo">> Repo</a></card>
+                <button className=' transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'><a className="App-link-repo" href="https://github.com/Cheez0id/Employee_CMS" alt="repo">> Repo</a></button></card>
            </section>
 
 
