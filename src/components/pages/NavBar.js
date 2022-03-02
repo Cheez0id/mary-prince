0c1c1e1b6fc00e2d5096913271a8340e93dd5230
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-03-02 11:43:51 -0500

    added video to porfolio

--- src/components/pages/NavBar.js
+++ src/components/pages/NavBar.js
@@ -5,53 +5,63 @@ import resume from "../../Assets/Resume-MLP-1pg.pdf";
 function NavBar({ currentPage, handlePageChange }) {
 	return (
 		<nav className="App-link  justify-center   mx-14   sm:py-5  inline-flex ">
-			
 			<ul className="abolute">
 				<li className="Nav-links">
-					<a
-						href="#home"
-						onClick={() => handlePageChange("Home")}
-					>
-						<img src={images.logoPic2} className="App-logo shadow-lg" alt="marylogo" width="150"/><img src={images.signature} className="shadow-lg" alt="marylogo" width="200"/>
-          </a>
+					<a href="#home" onClick={() => handlePageChange("Home")}>
+						<img
+							src={images.logoPic2}
+							className="App-logo shadow-lg"
+							alt="marylogo"
+							width="150"
+						/>
+						<img
+							src={images.signature}
+							className="shadow-lg"
+							alt="marylogo"
+							width="200"
+						/>
+						Full-Stack Developer
+					</a>
 				</li>
-				
+
+				<li className="Nav-links">
+					<a href="#Portflio" onClick={() => handlePageChange("Portfolio")}>
+						<span class="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 via-pink-500 to-purple-500 hover:no-underline Nav-links font-bold text-xl ">
+							Portfolio
+						</span>
+					</a>
+				</li>
 				<li className="Nav-links">
 					<a href="#AboutMe" onClick={() => handlePageChange("AboutMe")}>
 						About Me
 					</a>
 				</li>
-				<li className="Nav-links">
-        <a href="#Portflio" onClick={() => handlePageChange("Portfolio")}>
-				<span class="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 via-pink-500 to-purple-500 hover:no-underline Nav-links font-bold text-xl ">
-              Portfolio
-            </span>
-					</a>
-         </li> 
-          <li className="Nav-links font-bold">
-        <a href="#Contact" onClick={() => handlePageChange("Contact")}>
+				<li className="Nav-links font-bold">
+					<a href="#Contact" onClick={() => handlePageChange("Contact")}>
 						Contact
 					</a>
 				</li>
-				
-        <li className="Nav-links">
+
+				<li className="Nav-links">
 					<a href="https://github.com/Cheez0id">Github</a>
 				</li>
 				<li className="Nav-links">
-					<a href="https://cheez0id.github.io/MaryLeePrince-portfolio1/">Original Portfolio</a>
-				</li>
-				
-				<li className="Nav-links">
-					<a href={resume} download="Resume-MLP-1pg.pdf">
-                          
-						<button class="bg-gradient-to-r from-purple-800 to-teal-500 hover:from-pink-500 hover:to-teal-500 text-white py-2 px-2 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out shadow-lg absolute" type="button">
-                Download Resume
-              </button>
+					<a href="https://cheez0id.github.io/MaryLeePrince-portfolio1/">
+						Original Portfolio
 					</a>
 				</li>
-				
+
+				<li className="Nav-links">
+					<a href={resume} download="Resume-MLP-1pg.pdf">
+						<button
+							class="bg-gradient-to-r from-purple-800 to-teal-500 hover:from-pink-500 hover:to-teal-500 text-white py-2 px-2 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out shadow-lg absolute"
+							type="button"
+						>
+							Download Resume
+						</button>
+					</a>
+				</li>
 			</ul>
-			
 		</nav>
 	);
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -10,19 +10,20 @@ export default function Portfolio() {
                <h1><span class="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 via-pink-500 to-purple-500 hover:no-underline Nav-links font-bold shadow-lg">
               Personal Projects
             </span></h1>
-               <a className="App-link inline-flex" href="https://epimoodtracker.herokuapp.com/"><img src={images.epiMood} className="shadow-lg inlineImage" alt="epimoodlogo" width="150"/>
+               <a className="App-link inline-flex" href="https://epimoodtracker.herokuapp.com/"><img src={images.epiMood} className="shadow-lg inlineImage" alt="epimoodlogo" width="200"/>
                 </a>
                 <a className="App-link-repo" href="https://github.com/Cheez0id/EpiMood" alt="repo">> Repo</a>
+                <iframe src="https://drive.google.com/file/d/14flS4Oj-f50S8AWWzbh7lSEEbSpgph-c/preview" width="640" height="480" allow="autoplay"></iframe>
            </div>
 
            <div className='leading-loose longbox space-x-5 '>
            <h1><span class="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 via-pink-500 to-purple-500 hover:no-underline Nav-links font-bold shadow-lg">
               Group Projects
             </span></h1>
-               <a className="App-link inline-flex" href="https://cookies-for-coders-mary.herokuapp.com"><img src={images.cookies} className="shadow-lg inlineImage" alt="cookies" width="150"/>
+               <a className="App-link inline-flex" href="https://cookies-for-coders-mary.herokuapp.com"><img src={images.cookies} className="shadow-lg inlineImage" alt="cookies" width="200"/>
                 </a>
                 <a className="App-link-repo" href="https://github.com/Cheez0id/Full_Stack_Store" alt="repo">> Repo</a>
-           <a className="App-link inline-flex" href="https://cheez0id.github.io/Charity-Locator-Project-P1G7"><img src={images.foodFinder} className="shadow-lg inlineImage" alt="charitylocator" width="150"/>
+           <a className="App-link inline-flex" href="https://cheez0id.github.io/Charity-Locator-Project-P1G7"><img src={images.foodFinder} className="shadow-lg inlineImage" alt="charitylocator" width="200"/>
                 </a>
                 <a className="App-link-repo" href="https://github.com/Cheez0id/Full_Stack_Store" alt="repo">> Repo</a>
            </div>
@@ -30,14 +31,14 @@ export default function Portfolio() {
            <h1><span class="bg-clip-text text-transparent bg-gradient-to-r from-teal-400 via-pink-500 to-purple-500 hover:no-underline Nav-links font-bold shadow-lg">
               Coursework
             </span></h1>
-               <a className="App-link inline-flex" href="https://drive.google.com/file/d/15JwRFydPZ_h_6FmCOo9AcucGXar4Po4K/view?usp=sharing"><img src={images.social} className="shadow-lg inlineImage" alt="socialnetwork" width="150"/>
+               <a className="App-link inline-flex" href="https://drive.google.com/file/d/15JwRFydPZ_h_6FmCOo9AcucGXar4Po4K/view?usp=sharing"><img src={images.social} className="shadow-lg inlineImage" alt="socialnetwork" width="200"/>
                 </a>
                 <a className="App-link-repo" href="https://github.com/Cheez0id/social_network_API" alt="repo">> Repo</a>
-               <a className="App-link inline-flex" href="https://cheez0id.github.io/Weather-Dashboard-Homework"><img src={images.weather} className="shadow-lg inlineImage" alt="epimoodlogo" width="150"/>
+               <a className="App-link inline-flex" href="https://cheez0id.github.io/Weather-Dashboard-Homework"><img src={images.weather} className="shadow-lg inlineImage" alt="weather dashboard" width="200"/>
                 </a>
                 <a className="App-link-repo" href="https://github.com/Cheez0id/Weather-Dashboard-Homework" alt="repo">> Repo</a>
           
-               <a className="App-link inline-flex" href="https://drive.google.com/file/d/1NSTo07dxxTOB-uQCsocjfNLQFtiy8Y08/view?usp=sharing"><img src={images.employee} className="shadow-lg inlineImage" alt="epimoodlogo" width="150"/>
+               <a className="App-link inline-flex" href="https://drive.google.com/file/d/1NSTo07dxxTOB-uQCsocjfNLQFtiy8Y08/view?usp=sharing"><img src={images.employee} className="shadow-lg inlineImage" alt="employeeCMS" width="200"/>
                 </a>
                 <a className="App-link-repo" href="https://github.com/Cheez0id/Employee_CMS" alt="repo">> Repo</a>
            </div>
